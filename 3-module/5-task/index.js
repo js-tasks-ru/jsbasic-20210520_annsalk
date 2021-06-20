88e9739a601aec73c42146d656485ef669ff6de2
annsalk <annsalk@yandex.ru>
2021-06-20 11:45:20 +0700

    3modal 5task

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,25 @@
 function getMinMax(str) {
-  // ваш код...
+  /*let arr = str.split(' ');
+  let arr2 = arr.split(',');
+
+  let sortArr = arr2.sort( (a, b) => a - b );
+
+  let min = sortArr[0];
+  let max = sortArr[sortArr.length - 1];
+
+  return min;
+  return max;*/
+
+  let elements = str.split(/[\s,]+/)
+		.filter(el => !isNaN(el))
+		.map(el => parseFloat(el))
+		.sort((a, b) => a - b)
+
+	if( elements.legth == 0) {
+	return {}
+	} else if(elements.legth < 2) {
+	return {min: elements[0], max: elements[0]}
+	}
+
+	return {min: elements[0], max: elements[elements.length - 1]};
 }
