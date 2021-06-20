5d40550b125556616133e13ef33daf2f47c8a112
annsalk <annsalk@yandex.ru>
2021-06-20 17:49:14 +0700

    5-modal 1-task

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,9 @@
 function hideSelf() {
-  // ваш код...
+  //let btn = document.getElementsByClassName('hide-self-button');
+
+  let btn = document.querySelector('.hide-self-button');
+
+  btn.addEventListener('click', event => {
+    btn.hidden = true;
+  });
 }
